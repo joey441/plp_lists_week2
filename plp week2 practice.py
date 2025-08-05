8b95e6d3921c953a179238356fab130eb31b11ab
joey441 <joeymuiruri254@gmail.com>
2025-08-05 10:36:43 +0300

    first commit for the python practice

---
+++ plp week2 practice.py
@@ -0,0 +1,9 @@
+#Empty list
+my_list=[]
+my_list.extend([10,20,30,40])
+my_list.insert(1,15)
+second_list=[50,60,70]
+my_list.extend(second_list)
+my_list.remove(70)
+my_list.sort()
+print(my_list(30))
